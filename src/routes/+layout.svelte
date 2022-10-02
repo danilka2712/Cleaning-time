e61a6de6dcad4e8e4a9be6bdd7335300dd46248c
unknown <danilka2712@yandex.ru>
2022-10-02 13:17:02 +0600

    Initial commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -52,7 +52,7 @@
 					class="hover:bg-blue-300/40 duration-300 flex items-center justify-center rounded-full sm:w-10 sm:h-10"
 					href="https://vk.com/"
 				>
-				<Icon width='24' icon="eva:menu-fill" color="#33415c" />
+				<Icon width='28' icon="eva:menu-fill" color="#33415c" />
 				</a>
 			</div>
 		</div>
